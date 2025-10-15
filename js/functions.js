5f0a5472ba3fae51a2343a90ce8a9fcd7d8b887e
Ксения Хамитова <ksushahamitova@gmail.com>
2025-10-15 18:41:19 +0500

    Реализована функция проверки встречи

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,17 @@
-// Функция для проверки длины строки
-const checkLength = (text, maxLength) => text.length <= maxLength;
-
-// Примеры использования
-checkLength('проверяемая строка', 20); // true
-checkLength('проверяемая строка', 18); // true
-checkLength('проверяемая строка', 10); // false
-
-
-//Функция для проверки, является ли строка палиндромом
-const isPalindrome = (text) => {
-  const normalizedText = text.toLowerCase();
-  const reversedText = normalizedText.split('').reverse().join('');
-  return normalizedText === reversedText;
+const canHaveMeeting = (startWork, endWork, startMeeting, duration) => {
+  function timeToMinutes(timeStr) {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+  const startWorkMinutes = timeToMinutes(startWork);
+  const endWorkMinutes = timeToMinutes(endWork);
+  const startMeetingMinutes = timeToMinutes(startMeeting);
+  const endMeetingMinutes = startMeetingMinutes + duration;
+  return startMeetingMinutes >= startWorkMinutes && endMeetingMinutes <= endWorkMinutes;
 };
-isPalindrome('топот'); // true
-isPalindrome('ДовОд'); // true
-isPalindrome('Кекс');  // false
+
+canHaveMeeting('08:00', '17:30', '14:00', 90);
+canHaveMeeting('8:0', '10:0', '8:0', 120);
+canHaveMeeting('08:00', '14:30', '14:00', 90);
+canHaveMeeting('14:00', '17:30', '08:0', 90);
+canHaveMeeting('8:00', '17:30', '08:00', 900);
